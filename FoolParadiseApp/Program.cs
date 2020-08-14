53926486789d1b7e5f68110e0fa21bf2f8f5b529
Kufreabasi <engineer_kufre@yahoo.com>
2020-08-14 19:59:43 +0100

    completed assignment

--- FoolParadiseApp/Program.cs
+++ FoolParadiseApp/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Net.NetworkInformation;
-using System.Runtime.CompilerServices;
 
 namespace FoolParadiseApp
 {
@@ -24,72 +22,64 @@ namespace FoolParadiseApp
 
             Console.WriteLine(prog.Deposit(name, amount, interest, months));
             Console.Read();
-
             Console.WriteLine("Total expected amount is " + prog.Total(amount, interest, months));
 
         }
 
         public string Deposit(string name, decimal amount, decimal interest, int months)
         {
-
-            return $"Welcome {name}, thank for choosing us";
+            var total = Total(amount, interest, months);
+            var month = DateTime.Now.AddMonths(months).ToString("MMMM");
+            return $"Welcome {name}, your total sum, N{total} , should be ready in {month}. Thanks";
         }
 
         public decimal Total(decimal amount, decimal interest, int months)
         {
             try
             {
-                decimal total  =0 ;
-                //----- ur logic
-
-
+                decimal total = 0;
+                var interestAccrued = NetInterest(amount, interest, months);
+                total = amount + interestAccrued;
                 return total;
-
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-
                 return -1;
             }
-
         }
 
-        public decimal NetInterest(decimal interest, int months)
+        public decimal NetInterest(decimal amount, decimal interest, int months)
         {
             try
             {
                 decimal netInterest = 0;
-                //----- ur logic
-
-
+                netInterest = amount * (interest / 100) * months;
                 return netInterest;
-
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-
                 return -1;
             }
-
         }
 
-        public decimal Accrued(decimal interest, int months)
+        public decimal Accrued(decimal threshold, decimal amount, int months)
         {
             try
             {
-                decimal netInterest = 0;
-                //----- ur logic
-
-
-                return netInterest;
-
+                if (amount < threshold)
+                {
+                    return Total(amount, 50, months);
+                }
+                else
+                {
+                    return Total(amount, 100, months);
+                }
             }
-            catch (Exception ex)
+            catch (Exception)
             {
 
                 return -1;
             }
-
         }
     }
 
