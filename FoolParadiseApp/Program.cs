77ad52e44f7744f07ac445ef5827fb922637a497
solaadekan@gmail.com <solaadekan@gmail.com>
2020-08-14 20:13:06 +0100

    My solution Olusola Adekan

--- FoolParadiseApp/Program.cs
+++ FoolParadiseApp/Program.cs
@@ -1,103 +1,52 @@
-﻿using System;
+﻿using FoolParadiseApp;
+using System;
 using System.Net.NetworkInformation;
 using System.Runtime.CompilerServices;
 
-namespace FoolParadiseApp
+namespace FoolParadiseuser
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello customer!");
+            Console.Write("Welcome!. Kindly fill in the details below:\n");
 
-            var prog = new Program();
-            Console.WriteLine("Enter your name below");
+            //Initialize the Program
+
+            //Enter details
+            Console.WriteLine("Enter your name");
             var name = Console.ReadLine();
             Console.WriteLine("Enter amount");
             var amount = decimal.Parse(Console.ReadLine());
-            Console.WriteLine("Enter interest");
+            Console.WriteLine("Enter interest (in %)");
             var interest = decimal.Parse(Console.ReadLine());
             Console.WriteLine("Enter duration in months");
             var months = Int32.Parse(Console.ReadLine());
 
-            Console.WriteLine("Hello Customer");
+            var user = new FoolParadiseCalculator(name, amount, interest, months);
 
-            Console.WriteLine(prog.Deposit(name, amount, interest, months));
-            Console.Read();
-
-            Console.WriteLine("Total expected amount is " + prog.Total(amount, interest, months));
-
-        }
-
-        public string Deposit(string name, decimal amount, decimal interest, int months)
-        {
-
-            return $"Welcome {name}, thank for choosing us";
-        }
-
-        public decimal Total(decimal amount, decimal interest, int months)
-        {
             try
             {
-                decimal total  =0 ;
-                //----- ur logic
+                var output = user.Deposit(name, amount, interest, months);
+                Console.WriteLine(output);
 
+                Console.WriteLine();
+                Console.WriteLine("Total Interest");
 
-                return total;
+                var depositResult = user.Total(user.Amount, user.Interest, user.Months);
+                Console.WriteLine(depositResult);
 
+                Console.WriteLine();
+                Console.WriteLine("Net Interest");
+
+                var netInterest = user.NetInterest(user.Interest, user.Months);
+                Console.WriteLine(netInterest);
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-
-                return -1;
+                Console.WriteLine("Enter a valid month number: from 1 - 12");
             }
-
-        }
-
-        public decimal NetInterest(decimal interest, int months)
-        {
-            try
-            {
-                decimal netInterest = 0;
-                //----- ur logic
-
-
-                return netInterest;
-
-            }
-            catch (Exception ex)
-            {
-
-                return -1;
-            }
-
-        }
-
-        public decimal Accrued(decimal interest, int months)
-        {
-            try
-            {
-                decimal netInterest = 0;
-                //----- ur logic
-
-
-                return netInterest;
-
-            }
-            catch (Exception ex)
-            {
-
-                return -1;
-            }
-
         }
     }
-
-    //public class BankingObject
-    //{
-    //    public int months { get; set; }
-    //    public string name { get; set; }
-    //    public decimal interest { get; set; }
-    //    public decimal amount { get; set; }
-    //}
 }
--- FoolParadiseTest/SavingTest.cs
+++ FoolParadiseTest/SavingTest.cs
@@ -8,33 +8,36 @@ namespace FoolParadiseTest
         [TestMethod]
         public void DepositTest()
         {
-            var foolApp = new Program();
-            string depo = foolApp.Deposit("Segun",2000,100,5);
-
-            Assert.AreEqual($"(Welcome Segun, your total should be ready in December. Thanks)", depo);
+            //var foolApp = new Program();
+            var depot = new FoolParadiseCalculator("Segun", 2000, 100, 5);
+            string depo = depot.Deposit(depot.Name, depot.Amount, depot.Interest, depot.Months);
+            Assert.AreEqual($"Welcome Segun, your total should be ready in January. Thanks", depo);
         }
-      
+
         [TestMethod]
         public void NetInterestTest()
         {
-            var foolApp = new Program();
-            decimal intT = foolApp.NetInterest(50, 6);
+            //var foolApp = new Program();
+            var sut = new FoolParadiseCalculator("Tunde", 1000, 40, 6);
+            decimal intT = sut.NetInterest(sut.Interest, sut.Months);
             Assert.AreEqual(2400, intT);
         }
 
         [TestMethod]
         public void TotalTest()
         {
-            var foolApp = new Program();
-            decimal total = foolApp.Total(1000, 40, 6);
-            Assert.AreEqual(8400, total);
+            // var foolApp = new Program();
+            var sut = new FoolParadiseCalculator("chinazor", 1000, 40, 6);
+            decimal tot = sut.Total(sut.Amount, sut.Interest, sut.Months);
+            Assert.AreEqual(8400, tot);
         }
 
         [TestMethod]
         public void TwoTotalTest()
         {
-            var foolApp = new Program();
-            decimal total = foolApp.Total(1000, 40, 6);
+            //var foolApp = new Program();
+            var sut = new FoolParadiseCalculator("Nato", 1000, 40, 6);
+            decimal total = sut.Total(1000, 40, 6);
             var doubleTotal = total * 2;
             Assert.AreEqual(16800, doubleTotal);
         }
